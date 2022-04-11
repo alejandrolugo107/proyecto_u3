f567c3b77d335bde986a3d6926ae79008235f745
AlejandroLugo <49300290+alejandrolugo107@users.noreply.github.com>
2022-04-11 16:28:13 -0500

    form simplifyied

--- src/components/Altas.js
+++ src/components/Altas.js
@@ -8,32 +8,20 @@ function Altas() {
                 <div class="col-md-4">
                     <label for="descripcion" className="form-label">Descripcion</label>
                     <input type="text" className="form-control" id="descripcion" required/>
-                    <div className="valid-feedback">
-                    Looks good!
-                    </div>
                 </div>
                 <div class="col-md-4">
                     <label for="validationCustom02" class="form-label">SKU</label>
-                    <input type="text" class="form-control" id="validationCustom02" value="Otto" required/>
-                    <div class="valid-feedback">
-                    Looks good!
-                    </div>
+                    <input type="number" class="form-control" id="validationCustom02" required/>
                 </div>
                 <div class="col-md-2">
                     <label for="validationCustomUsername" class="form-label">Cantidad</label>
                     <div class="input-group has-validation">
                     <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required/>
-                    <div class="invalid-feedback">
-                        Please choose a username.
-                    </div>
                     </div>
                 </div>
                 <div class="col-md-2">
                     <label for="validationCustom03" class="form-label">Marca</label>
                     <input type="text" class="form-control" id="validationCustom03" required/>
-                    <div class="invalid-feedback">
-                    Please provide a valid brand.
-                    </div>
                 </div>
             
                 <div class="col-12">
